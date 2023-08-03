73db11deeca34849718e932f41de661320e3a2bd
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-08-03 14:58:27 +0300

    update

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+  selectEl: document.querySelector('.breed-select'),
+  loaderEl: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  catInfoEl: document.querySelector('.cat-info'),
+};
