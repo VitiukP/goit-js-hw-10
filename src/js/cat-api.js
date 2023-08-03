5fe495c4b7cfe9070e5110f6edc9fbac2e3d83dd
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-08-03 15:02:23 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
-  'live_ENwvfA1ajvZtHtuPVtVujcLFFwtoZPMdF4b16yIiS25NiI21sc6WFvUoSaP0HfUV';
+  'live_gz9oBrIDOxSbfV6auru1158Y1wpb0wErN31Tjm5XN9NhJvTGHK95WxPdPGKKPuJG';
 const API_URL = 'https://api.thecatapi.com/v1/breeds';
 const SEARCH_URL = `https://api.thecatapi.com/v1/images/search`;
 function fetchBreeds() {
